81708fcc4d7765bba5b80cd29ff2f722677c8c8f
shivansh <shivansh123321@gmail.com>
2023-04-10 13:03:12 +0530

    added heathcheck with logs

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 from flask import Flask
-from flask import Blueprint
+import requests
+import time
 
 app = Flask(__name__)
-health_bp = Blueprint('health', __name__)
 
+@app.route('/health')
+def health_check():
+    return {'status': 'OK'}, 200
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is version 2 - with health check'
-
-@health_bp.route('/healthcheck')
-def health_check():
-    return 'OK', 200
-
-def initialize_app(flask_app):
-    app.register_blueprint(health_bp)
+    return 'Hello, World! This is version 2 - with healh check 2'
 
 
 if __name__ == "__main__":
-    initialize_app(app)
     app.run(host='0.0.0.0',debug=True,port=8082)
-    
 
+
+
+while True:
+    response = requests.get('http://localhost:8082/health')
+    if response.status_code == 200:
+        print('Flask app is healthy')
+    else:
+        print('Flask app is not healthy')
+    time.sleep(30)
