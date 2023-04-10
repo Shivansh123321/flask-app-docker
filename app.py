7622715aea279effcbf948ea87e116798f880dd5
shivansh <shivansh123321@gmail.com>
2023-04-10 13:26:07 +0530

    added health logs

--- app.py
+++ app.py
@@ -1,27 +1,20 @@
 from flask import Flask
-import requests
-import time
+import logging
 
 app = Flask(__name__)
 
 @app.route('/health')
 def health_check():
+    logging.info("in health check")
     return {'status': 'OK'}, 200
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is version 2 - with healh check 2'
+    logging.info("in hellow world")
+    return 'Hello, World! This is version 2 - with healh check 3'
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True,port=8082)
 
 
-
-while True:
-    response = requests.get('http://localhost:8082/health')
-    if response.status_code == 200:
-        print('Flask app is healthy')
-    else:
-        print('Flask app is not healthy')
-    time.sleep(30)
